9749aab751d7077f12288e7343349343c483e464
Mara Daniela López Pérez <m.lopezp171003@gmail.com>
2025-01-30 18:30:21 -0600

    Pagina base

---
+++ EcommerceCafeMendez/CafeMendez.UI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CafeMendez.UI.Startup))]
+namespace CafeMendez.UI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
